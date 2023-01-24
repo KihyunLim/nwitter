775762c1269eae6334c4cf4298f85bcaf8fb5c32
khlim <khlim@home.com>
2023-01-25 01:00:24 +0900

    Updates

---
+++ precache-manifest.b1e79bc9cb5b95ddaa70e8e161edb9d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91a86a6426ae2e63dbe85218149971f0",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "0e8802ee307b21016025",
+    "url": "/nwitter/static/css/main.afa4deda.chunk.css"
+  },
+  {
+    "revision": "69cd62178532cae13380",
+    "url": "/nwitter/static/js/2.596ea16a.chunk.js"
+  },
+  {
+    "revision": "e344263dd6d83060e8b35e50639b1991",
+    "url": "/nwitter/static/js/2.596ea16a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e8802ee307b21016025",
+    "url": "/nwitter/static/js/main.4c96408d.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/nwitter/static/js/main.4c96408d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "125d1d768f4e9614d81a",
+    "url": "/nwitter/static/js/runtime-main.3a662223.js"
+  }
+]);
